ef77fe91ac6f8167afa00132f4ac7d400c81a607
keith bonarrigo <keith@keithbonline.com>
2018-05-13 22:42:40 -0700

    adding recursive checks to subsequent pages

--- env/google-check-results.py
+++ env/google-check-results.py
@@ -4,10 +4,30 @@ from gsearch import *
 
 driver = webdriver.Chrome(executable_path=r'C:\selenium\chromedriver_win32\chromedriver.exe')
 driver.get('http://www.google.com') #open our URL
-myterms = ['autism counseling in bothell', 'EMDR Bothell'] #our search terms - duh
-
+myterms = [
+    'Asperger Syndrome Screening for adults bothell wa',
+    'Autism spectrum screening for adults wa',
+    'Asperger Syndrome Screening for adults seattle',
+    'Autism spectrum screening for adults bothell',
+    'Autism spectrum screening for adults wa ',
+    'Autism spectrum screening for adults seattle',
+    'A.S.D. screening for adults wa',
+    'A.S.D. screening for adults bothell wa',
+    'A.S.D. screening for adults seattle',
+    'counseling for autism wa',
+    'counseling for autism bothell',
+    'counseling for autism seattle',
+    'counseling for couples with a member on the spectrum',
+    'counseling for couples with a member on the spectrum wa',
+    'counseling for couples with a member on the spectrum bothell',
+    'anxiety and depression counseling wa',
+    'anxiety and depression counseling bothell',
+    'anxiety and depression counseling seattle',
+    'developing accommodations for the workplace wa',
+    'developing accommodations for the workplace bothell',
+    'developing accommodations for the workplace seattle'
+] #our search terms
 g = gsearch(myterms, driver)
-
 
 
 
--- env/gsearch.py
+++ env/gsearch.py
@@ -1,46 +1,81 @@
 import time
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 class gsearch:
     def __init__(self, myterms, driver):
         self.myterms = myterms
         self.driver = driver
-        self.mypage = 1
+        self.mypage = 1 # this is the page counter for the google search results
+        self.pager = 3
+        self.report = []
 
         for term in self.myterms:
+            self.mypage = 1
+            print("------------------------------------------------------------")
             self.sendthesearch(term)
             self.findsearchterm(term)
+            print("------------------------------------------------------------")
+
+        for ireport in self.report:
+            print(ireport)
 
     def sendthesearch(self,term):
         elem = self.driver.find_element_by_name("q")  # google search bar
-        time.sleep(1)
+        elem.clear()
+        time.sleep(0)
         elem.send_keys(term)
         elem.send_keys(Keys.RETURN)
-        time.sleep(1)
-        print('go')
+        time.sleep(0)
 
     def findsearchterm(self, term):
         mycounter = int(0)  # basic counter to track the link position
-        self.mypage = 1  # this is the page counter for the google search results
-        print("Testing " + term + ":")
+        print("Testing '" + term + "' at page " + str(self.mypage) + ":")
         div = self.driver.find_element_by_class_name('srg')  # this is the elements that houses the links we're interested in
         found = 0  # track our link count to see if we need to move to the next page
         for a in div.find_elements_by_xpath('.//a'):
             thislink = str(a.get_attribute('href'))
-
             if ('google.com' in thislink) | ('webcache.googleusercontent.com' in thislink):
-                # do nothing
-                mycounter = mycounter
+                mycounter = mycounter #do nothing
             else:
                 mycounter = mycounter + 1
                 showcounter = str(mycounter)
             if 'http://counselinginbothell.com' in thislink:
                 showcounter = str(mycounter)
+                message = thislink + " at position " + showcounter + " at page " + str(self.mypage)
                 print(thislink + " at position " + showcounter + " at page " + str(self.mypage))
+                self.report.append(message)
                 found = found + 1
-
-        if found < 1:
+        if found < 1 & self.mypage < self.pager:
             print('no results for ' + term + " on page " + str(self.mypage))
+            found = self.checknextpages(term, found)
+            if found == 0:
+                self.report.append("No listings for '"+ term + "' in " + str(self.pager) + " pages")
+
+        self.driver.back()
+        time.sleep(1)
+        return found
+
+    def checksubsequentpage(self, term):
+        print("checking page "+ str(self.mypage))
+        elem = self.driver.find_element_by_link_text(str(self.mypage))
+        elem.click()
+        time.sleep(2)
+        found = self.findsearchterm(term)
+        if found == 1:
+            return 1
         else:
-            self.driver.back()
-            time.sleep(3)
+            return 0
+
+
+    def checknextpages(self, term, found):
+        if self.mypage < self.pager:
+            self.mypage = self.mypage+1
+            self.checksubsequentpage(term)
+            self.checknextpages(term, found)
+            return 0
+        elif self.mypage==self.pager:
+            return 1
+        else:
+            return 1
+        #self.driver.back()
