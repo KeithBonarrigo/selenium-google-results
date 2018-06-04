5f927168abeda4d92f0a6f71c36967aea8ee21de
keith bonarrigo <keith@keithbonline.com>
2018-06-03 22:01:49 -0700

    adding enhanced logging

--- env/google-check-results.py
+++ env/google-check-results.py
@@ -5,29 +5,36 @@ from gsearch import *
 driver = webdriver.Chrome(executable_path=r'C:\selenium\chromedriver_win32\chromedriver.exe')
 driver.get('http://www.google.com') #open our URL
 myterms = [
-    'Asperger Syndrome Screening for adults bothell wa',
-    'Autism spectrum screening for adults wa',
-    'Asperger Syndrome Screening for adults seattle',
-    'Autism spectrum screening for adults bothell',
-    'Autism spectrum screening for adults wa ',
-    'Autism spectrum screening for adults seattle',
-    'A.S.D. screening for adults wa',
-    'A.S.D. screening for adults bothell wa',
-    'A.S.D. screening for adults seattle',
-    'counseling for autism wa',
-    'counseling for autism bothell',
-    'counseling for autism seattle',
-    'counseling for couples with a member on the spectrum',
-    'counseling for couples with a member on the spectrum wa',
-    'counseling for couples with a member on the spectrum bothell',
-    'anxiety and depression counseling wa',
-    'anxiety and depression counseling bothell',
-    'anxiety and depression counseling seattle',
-    'developing accommodations for the workplace wa',
-    'developing accommodations for the workplace bothell',
-    'developing accommodations for the workplace seattle'
+    'EMDR bothell',
+    'EMDR seattle',
+    'EMDR wa',
+    'EMDR Therapy bothell',
+    'EMDR Therapy seattle',
+    'EMDR Therapy wa',
+    'EMDR Treatment bothell',
+    # 'Asperger Syndrome Screening for adults bothell wa',
+    # 'Autism spectrum screening for adults wa',
+    # 'Asperger Syndrome Screening for adults seattle',
+    # 'Autism spectrum screening for adults bothell',
+    # 'Autism spectrum screening for adults wa ',
+    # 'Autism spectrum screening for adults seattle',
+    # 'A.S.D. screening for adults wa',
+    # 'A.S.D. screening for adults bothell wa',
+    # 'A.S.D. screening for adults seattle',
+    # 'counseling for autism wa',
+    # 'counseling for autism bothell',
+    # 'counseling for autism seattle',
+    # 'counseling for couples with a member on the spectrum',
+    # 'counseling for couples with a member on the spectrum wa',
+    # 'counseling for couples with a member on the spectrum bothell',
+    # 'anxiety and depression counseling wa',
+    # 'anxiety and depression counseling bothell',
+    # 'anxiety and depression counseling seattle',
+    # 'developing accommodations for the workplace wa',
+    # 'developing accommodations for the workplace bothell',
+    # 'developing accommodations for the workplace seattle'
 ] #our search terms
-g = GoogleSearch(myterms, driver)
+g = GoogleSearch(myterms, driver, 4, 'http://counselinginbothell.com')
 
 
 
--- env/gsearch.py
+++ env/gsearch.py
@@ -5,19 +5,28 @@ from selenium.webdriver.common.keys import Keys
 from random import randint
 
 class GoogleSearch:
-    def __init__(self, myterms, driver):
+    def __init__(self, myterms, driver, pager, url):
+        self.url_to_look_for = url
         self.myterms = myterms
         self.driver = driver
         self.mypage = 1 # this is the page counter for the google search results
-        self.pager = 3
-        self.report = []
-
+        self.pager = pager #number of pages to check
+        self.report = [] #this is a container for the info we'll be logging
+        self.report.append("") #set up our title slot for the report
         today = datetime.datetime.today().strftime('%m-%d-%Y')
-        today_string = "SEO REPORT FOR " + str(today)
-        #self.report.append("------------------------------------------------------------")
+        today_string = "SEO report for " + self.url_to_look_for + " on " + str(today)
+        self.report[0] = today_string
+        self.not_found = [] #container for the terms that don't show up
 
-        self.report.append(today_string)
-        for term in self.myterms:
+        page_array_added = 0 #loop_counter
+        while page_array_added < self.pager:
+            list_to_insert = [] #set up and empty container
+            self.report.append(list_to_insert ) #add it to the larger report list
+            page_array_added += 1
+
+        #print(self.report)
+
+        for term in self.myterms: #now loop through our terms and see where they appear
             self.mypage = 1
             print("------------------------------------------------------------")
             self.sendthesearch(term)
@@ -26,10 +35,32 @@ class GoogleSearch:
 
         report_name = 'reportfile_' + str(today) + '.txt'
         F = open(report_name,'w+')
-        for ireport in self.report:
-            print(ireport)
-            F.write(ireport)
+
+        counter = 0
+        F.write(today_string) #write the title
+        F.write('\r\n')
+        F.write('\r\n')
+
+        for ireport in self.report: #now write the results that showed up
+            if counter > 0:
+                F.write("Terms appearing on page " + str(counter) + ":")
+                F.write('\r\n')
+                F.write("----------------------------------------------------------------")
+                F.write('\r\n')
+                for line in ireport:
+                    F.write(line)
+                    F.write('\r\n')
+                F.write('\r\n')
+            counter += 1
+
+        F.write(str(len(self.not_found)) + " terms not found in " + str(self.pager) + " pages:") #now write the results that don't appear
+        F.write("----------------------------------------------------------------")
+        F.write('\r\n')
+        for n in self.not_found:
+            F.write(n)
             F.write('\r\n')
+
+        F.close()
 
     def get_sleep_random(self, start, stop):
         return(randint(start, stop))
@@ -48,7 +79,14 @@ class GoogleSearch:
         mycounter = int(0)  # basic counter to track the link position
         test_message = "Testing '" + term + "' at page " + str(self.mypage) + ":"
         print(test_message)
-        self.report.append(test_message)
+        #print('report is ' + str(len(self.report)) + "long")
+        #print(self.report)
+
+        if len(self.report)<self.mypage:
+            print('creating ' + str(self.mypage))
+            push_list = []
+            #self.report.append.push_list
+
         div = self.driver.find_element_by_class_name('srg')  # this is the elements that houses the links we're interested in
         found = 0  # track our link count to see if we need to move to the next page
         for a in div.find_elements_by_xpath('.//a'):
@@ -57,20 +95,19 @@ class GoogleSearch:
                 continue
             else:
                 mycounter = mycounter + 1
+            if self.url_to_look_for in thislink:
                 showcounter = str(mycounter)
-            if 'http://counselinginbothell.com' in thislink:
-                showcounter = str(mycounter)
-                message = term + " found at position " + showcounter + " at page " + str(self.mypage) + " " + "(" + thislink + ")"
-                print(thislink + " at position " + showcounter + " at page " + str(self.mypage))
-                self.report.append(message)
+                message = "\"" + term + "\"" + " ---------------- found at position " + showcounter + " " + "(" + thislink + ")"
+                print(message)
+                self.report[self.mypage].append(message)
                 found = found + 1
         if found < 1 & self.mypage < self.pager:
             no_results_message = 'no results for ' + term + " on page " + str(self.mypage)
             print(no_results_message)
-            self.report.append(no_results_message)
+            #self.report.append(no_results_message)
             found = self.checknextpages(term, found)
             if found == 0:
-                self.report.append("No listings for '"+ term + "' in " + str(self.pager) + " pages")
+                self.not_found.append(term)
 
         self.driver.back()
         rand = self.get_sleep_random(1, 4)
